730626998109d11d2e90477021b343869c77146c
jgfranco17 <chino.franco@gmail.com>
2023-05-18 19:36:43 +0900

    Removed stdout print

--- stockpyle/utils.py
+++ stockpyle/utils.py
@@ -5,6 +5,4 @@ import datetime as dt
 def get_pause():
     now = dt.datetime.now()
     next_min = now.replace(second=0, microsecond=0) + dt.timedelta(minutes=1)
-    pause = math.ceil((next_min - now).seconds)
-    print(f'Sleep for {pause}s...')
-    return pause
+    return math.ceil((next_min - now).seconds)
