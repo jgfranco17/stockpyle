54dd56b29d22d0efc4da910072aa50ff5ebb7615
jgfranco17 <chino.franco@gmail.com>
2023-05-18 16:29:36 +0900

    Added `TradeLog` and `Transaction` classes

--- capytal/models.py
+++ capytal/models.py
@@ -1,9 +1,10 @@
+import datetime as dt
 import yfinance as yf
 from typing import List
 from dataclasses import dataclass, field
 
 
-@dataclass(frozen=True)
+@dataclass
 class Asset:
     symbol: str
     holding: bool = False
@@ -14,11 +15,42 @@ class Asset:
         self.ticker = yf.Ticker(self.symbol)
 
 
+@dataclass
+class Transaction:
+    date: dt.datetime
+    ticker: str
+    side: str
+    price: float
+
+    def __post_init__(self):
+        self.ticker = self.ticker.upper()
+        self.side = self.side.lower()
+        self.price = round(self.price, 2)
+
+
 class AssetCollection:
-    def __post_init__(self, symbols: List[str]):
+    def __init__(self, symbols: List[str]):
         self.__symbols = symbols
         self.__assets = [Asset(symbol=symbol) for symbol in self.__symbols]
 
     def add(self, symbol: str):
         new_asset = Asset(symbol=symbol.upper())
         self.__assets.append(new_asset)
+
+
+class TradeLog:
+    def __init__(self) -> None:
+        self.__buy_log = []
+        self.__sell_log = []
+
+    @property
+    def buy_count(self) -> int:
+        return len(self.__log)
+
+    def update(self, transaction: Transaction):
+        if transaction.side == "buy":
+            self.__buy_log.append(transaction)
+        if transaction.side == "sell":
+            self.__sell_log.append(transaction)
+        else:
+            raise ValueError(f'Expecting \"buy\" or \"sell\", got \"{transaction.side}\"')
