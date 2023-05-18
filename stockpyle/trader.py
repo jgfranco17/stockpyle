f8760c36ec92016598d54f2a8296390211a9484a
jgfranco17 <chino.franco@gmail.com>
2023-05-18 18:45:15 +0900

    Refactored logic for transaction logging

--- stockpyle/trader.py
+++ stockpyle/trader.py
@@ -37,39 +37,39 @@ class SingleAssetTrader:
 
                 df['SMA_fast'] = ta.sma(df['Close'], self.__interval_fast)
                 df['SMA_slow'] = ta.sma(df['Close'], self.__interval_slow)
-
+                new_recommendation_created = False
+                side = None
                 price = df.iloc[-1]['Close']
+
                 if df.iloc[-1]['SMA_fast'] > df.iloc[-1]['SMA_slow'] and not self.__currently_holding:
                     print(f'Buy {self.__symbol} @ ${price:.2f}')
-                    transaction_details = {
-                        "date": dt.datetime.now(),
-                        "ticker": self.__symbol,
-                        "side": "buy",
-                        "price": price
-                    }
-                    transaction = Transaction(**transaction_details)
-                    self.__tradelog.update(transaction)
                     self.__currently_holding = True
+                    side = "buy"
+                    new_recommendation_created = True
 
                 elif df.iloc[-1]['SMA_fast'] < df.iloc[-1]['SMA_slow'] and self.__currently_holding:
                     print(f'Sell {self.__symbol} @ ${price:.2f}')
+                    self.__currently_holding = False
+                    side = "sell"
+                    new_recommendation_created = True
+
+                else:
+                    print(f'Currently no recommendations for {self.__symbol} stocks.')
+
+                if new_recommendation_created:
                     transaction_details = {
                         "date": dt.datetime.now(),
                         "ticker": self.__symbol,
-                        "side": "buy",
+                        "side": side,
                         "price": price
                     }
                     transaction = Transaction(**transaction_details)
                     self.__tradelog.update(transaction)
-                    self.__currently_holding = False
-
-                else:
-                    print(f'Currently no recommendations for {self.__symbol} stocks.')
 
                 time.sleep(get_pause())
 
             except KeyboardInterrupt:
                 self.__is_running = False
                 print("Shutting down trading bot!")
-                self.__tradelog.export(format="json")
+                self.__tradelog.export()
                 break
